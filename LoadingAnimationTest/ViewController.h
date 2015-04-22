3b7db9125d6892ba61faeba14b04fa0ed254c40c
administrator <administrator@administratordeMacBook-Pro.local>
2015-04-22 15:55:39 +0800

    Initial Commit

---
+++ LoadingAnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadingAnimationTest
+//
+//  Created by administrator on 15/4/22.
+//  Copyright (c) 2015年 administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
